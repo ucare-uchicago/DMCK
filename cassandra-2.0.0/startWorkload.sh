0cedfa5f65ecd104065fe664749e2d780e0b7adc
Jeffrey F. Lukman <jeffreyflukman@gmail.com>
2018-02-12 21:13:46 -0800

    Refine startWorkload.sh for cassandra-6023.

--- cassandra-2.0.0/startWorkload.sh
+++ cassandra-2.0.0/startWorkload.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ $# != 2 ]; then
-  echo "Usage: startWorkload.sh <data_key> <test_id>" > workload_error.log
+if [ $# != 1 ]; then
+  echo "Usage: startWorkload.sh <test_id>" > workload_error.log
 	exit 1m
 fi
 
 . ./readconfig
 
-data_key=$1
-test_id=$2
+test_id=$1
+workload_name="cass-6013"
 
 classpath=$working_dir:$target_sys_dir/build/classes/main:$target_sys_dir/build/classes/thrift
 driverLib=$target_sys_dir/datastax_lib
@@ -16,5 +16,5 @@ for i in `ls $driverLib/*.jar`; do
 	classpath=$classpath:$i
 done
 
-java -cp $classpath -Dlog4j.configuration=cass_log.properties edu.uchicago.cs.ucare.dmck.interceptor.WorkloadExecutor $data_key > $working_dir/console/$test_id/workload.log &
+java -cp $classpath -Dlog4j.configuration=cass_log.properties edu.uchicago.cs.ucare.dmck.interceptor.WorkloadExecutor $workload_name > $working_dir/console/$test_id/workload.log &
 echo $! > $working_dir/samc-workload.pid
